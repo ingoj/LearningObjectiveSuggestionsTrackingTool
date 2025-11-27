a24794ef31b0d5dccee1fdda1bac489bfd8868db
Ioanna Mitroulaki <ioanna.mitroulaki@kroepelin-projekte.de>
2025-11-27 12:07:50 +0100

    Update plugin Version

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 $id = 'dhbwtrcktl';
-$version = '2.1.0'; // TODO version 2.2.0 or 3.0.0 ???
-$ilias_min_version = "10.0";
-$ilias_max_version = "10.999";
-$responsible = "Kröpelin Projekt GmbH ";
-$responsible_mail = "info@kroepelin-projekte.de";
+$version = '2.2.0';
+$ilias_min_version = '10.0';
+$ilias_max_version = '10.999';
+$responsible = 'Kröpelin Projekt GmbH';
+$responsible_mail = 'info@kroepelin-projekte.de';
